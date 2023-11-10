96f81bbb717e52df66323e87fcf6b83b7148c148
MrFabiioo <fabiolin57@gmail.com>
2023-11-10 10:45:35 -0500

    Aplicando persistencia con loccalstorage

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
-const defaultTodos =[
-  {text:'Cortar cebello',completed:true},
-  {text:'sacar reciclaje',completed:false},
-  {text:'leer 15 min ',completed:false},
-  {text:'hacer ejercicio',completed:false},
-  {text:'hacer guacamole',completed:true}
-];
+// const defaultTodos =[
+//   {text:'Cortar cebello',completed:true},
+//   {text:'sacar reciclaje',completed:false},
+//   {text:'leer 15 min ',completed:false},
+//   {text:'hacer ejercicio',completed:false},
+//   {text:'hacer guacamole',completed:true}
+// ];
+
+//localStorage.setItem('TODOS_V1', defaultTodos);
+
+//localStorage.removeItem('TODOS_V1');
+
 
 
 function App() {
-  const[todos,setTodos]= React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos=[]
+  } else {
+    parsedTodos=JSON.parse(localStorageTodos);
+  }
+  
+  const[todos,setTodos]= React.useState(parsedTodos);
   const [searchValue,setSearchValue]= React.useState('');
   //console.warn(`Se esta buscando: ${searchValue}`)
 
@@ -30,13 +44,21 @@ function App() {
     return todoText.includes(searchText);
   });
 
+  const saveTodos = (newTodos) =>{
+    localStorageTodos.setItem('TODOS_V1',JSON.stringif(newTodos));
+    setTodos(newTodos);
+  };
+
+  // const handleAddingTodos=(newTodo)=> {
+  // }
+
   const completeTodos= (text)=>{
     const newTodos =[...todos];
     const todoIndex=newTodos.findIndex(
       (todo) => todo.text === text
     );
     newTodos[todoIndex].completed=true;
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
 
   const deleteTodos= (text)=>{
@@ -45,7 +67,7 @@ function App() {
       (todo) => todo.text === text
     );
     newTodos.splice(todoIndex,1);
-    setTodos(newTodos)
+    saveTodos(newTodos)
   }
   return (
   <React.Fragment>
