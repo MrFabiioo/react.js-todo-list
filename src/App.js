4680edd8390ce47c48c714c24a527c6e65dab0de
MrFabiioo <fabiolin57@gmail.com>
2023-11-10 11:06:02 -0500

    aplicando customHook al local storage para minimizar la logica con useLocalStorage

--- src/App.js
+++ src/App.js
@@ -18,19 +18,28 @@ import React from 'react';
 
 //localStorage.removeItem('TODOS_V1');
 
+function useLocalStorage(itemName, initialValue){
 
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem('TODOS_V1',JSON.stringify(initialValue));
+    parsedItem=[]
+  } else {
+    parsedItem=JSON.parse(localStorageItem);
+  }
+  const[item,setItem]=React.useState(parsedItem);
+  const saveItem = (newItem) =>{
+    localStorageItem.setItem('TODOS_V1',JSON.stringif(newItem));
+    setItem(newItem);
+  };
+  return [saveItem, item]
+}
 
 function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    parsedTodos=[]
-  } else {
-    parsedTodos=JSON.parse(localStorageTodos);
-  }
+
   
-  const[todos,setTodos]= React.useState(parsedTodos);
+  const[todos,saveTodos]= useLocalStorage('TODOS_V1',[]);
   const [searchValue,setSearchValue]= React.useState('');
   //console.warn(`Se esta buscando: ${searchValue}`)
 
@@ -43,14 +52,6 @@ function App() {
     const searchText =searchValue.toLocaleLowerCase();
     return todoText.includes(searchText);
   });
-
-  const saveTodos = (newTodos) =>{
-    localStorageTodos.setItem('TODOS_V1',JSON.stringif(newTodos));
-    setTodos(newTodos);
-  };
-
-  // const handleAddingTodos=(newTodo)=> {
-  // }
 
   const completeTodos= (text)=>{
     const newTodos =[...todos];
