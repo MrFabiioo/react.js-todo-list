636dd742596de569c24917517e4ac138e585da80
MrFabiioo <fabiolin57@gmail.com>
2023-11-17 10:13:24 -0500

    añadiendo las funcionalidades de: abrir y cerrar el MODAL por medio de eventos onClick y agregando la funcionalidad de agregar todos por medio del todoCOntext

--- src/TodoContext/TodoContext.jsx
+++ src/TodoContext/TodoContext.jsx
@@ -20,6 +20,15 @@ function  TodoProvider({children}){
         return todoText.includes(searchText);
     });
 
+    const addTodo= (text)=>{
+        const newTodos =[...todos];
+        newTodos.push({
+            text,
+            completed: false
+        });
+        saveTodos(newTodos)
+    }
+
     const completeTodos= (text)=>{
         const newTodos =[...todos];
         const todoIndex=newTodos.findIndex(
@@ -47,6 +56,7 @@ function  TodoProvider({children}){
         completeTodos,
         deleteTodos,
         loading,
+        addTodo,
         openModal,
         setOpenModal,
         error}}>
--- src/TodoForm/TodoForm.jsx
+++ src/TodoForm/TodoForm.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import './TodoForm.css'
+import { TodoContext } from "../TodoContext/TodoContext";
+
+
 
 function TodoForm(){
+    const {setOpenModal,addTodo} = React.useContext(TodoContext);
+    const [newTodoValue,setNewTodoValue] = React.useState('');
+    const onSubmit =(event)=>{
+        event.preventDefault();
+        addTodo(newTodoValue)
+        setOpenModal(false);
+    }
+    const onCancel=()=>{
+        setOpenModal(false);
+    }
+    const onChange=(event)=>{
+        setNewTodoValue(event.target.value);
+    }
     return(
-        <form onSubmit={(event)=>{
-            event.preventDefault()
-        }}>
+        <form>
             <label>Escribe tu nuevo TODO</label>
-            <textarea placeholder="cortarcebolla">
+            <textarea value={newTodoValue} onChange={onChange} placeholder="cortarcebolla">
                 
             </textarea>
             <div className="TodoForm-ButtonContainer">
-            <button type="submit" className="TodoForm-button TodoForm-button--cancel">Cancelar </button>
-            <button type="submit" className="TodoForm-button TodoForm-button--add">Agregar todo</button>
+            <button onClick={onCancel} type="submit" className="TodoForm-button TodoForm-button--cancel">Cancelar </button>
+            <button onClick={onSubmit} type="submit" className="TodoForm-button TodoForm-button--add">Agregar todo</button>
             </div>
         </form>
     );
